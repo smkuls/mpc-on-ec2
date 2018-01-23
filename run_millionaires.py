55e9a5f8a8c671f2ba32268591c960f8b0db7b01
Samarth Kulshreshtha <samarth5@illinois.edu>
2018-01-22 20:59:15 -0600

    Changed to asyncmix branch of amiller/viff, moved setting of environment variable to setup.sh

--- run_millionaires.py
+++ run_millionaires.py
@@ -6,19 +6,16 @@ def run_viff_millionaires(instance_ids, instance_ips):
     port_numbers = [9000+i for i in range(1, ec2Manager.config.VM_COUNT + 1)]
 
     # kill_previous_commands = "killall python"
-    viff_export_command =  "echo 'export PYTHONPATH=$PYTHONPATH:$HOME/opt/lib/python' >> ~/.bashrc"
-    source_bashrc_command = "source ~/.bashrc"
     for player_number, instance_id in zip(range(1, ec2Manager.config.VM_COUNT + 1), instance_ids):
-        viff_config_command = "python ~/viff-f1d477e94d0b/apps/generate-config-files.py -n "+str(ec2Manager.config.VM_COUNT)+" -t 1"
+        viff_config_command = "python ~/viff/apps/generate-config-files.py -n "+str(ec2Manager.config.VM_COUNT)+" -t 1"
         for port_number, instance_ip in zip(port_numbers, instance_ips):
             viff_config_command += " " + instance_ip+":"+str(port_number)
 
-        viff_app_command = "python ~/viff-f1d477e94d0b/apps/millionaires.py --no-ssl player-"+str(player_number)+".ini > output 2>&1 &"
+        viff_app_command = "python ~/viff/apps/millionaires.py --no-ssl player-"+str(player_number)+".ini > output 2>&1 &"
 
         # print (viff_config_command)
         # print (viff_app_command)
-        ec2Manager.execute_command_on_instance(instance_id, [viff_export_command, source_bashrc_command,
-            viff_config_command, viff_app_command])
+        ec2Manager.execute_command_on_instance(instance_id, [viff_config_command, viff_app_command])
 
 def display_output(instance_ids):
     for instance_id in instance_ids:
--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo yum -y update
-sudo yum -y install git gcc gmp-devel
+sudo yum -y install git gcc gmp-devel git
 sudo pip install twisted[tls]
 sudo pip install gmpy
 sudo pip uninstall -y pyasn1
 sudo easy_install pyasn1
-wget http://hg.viff.dk/viff/archive/tip.zip
-unzip tip.zip
-mv viff-f1d477e94d0b/ /home/ec2-user
-cd /home/ec2-user/viff-f1d477e94d0b
+git clone -b asyncmix https://github.com/amiller/viff/
+mv viff/ /home/ec2-user
+cd /home/ec2-user/viff
 python setup.py install --home=/home/ec2-user/opt
+echo 'export PYTHONPATH=$PYTHONPATH:/home/ec2-user/opt/lib/python' >> /home/ec2-user/.bashrc
+source /home/ec2-user/.bashrc
